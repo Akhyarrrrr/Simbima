df2fb4545fb8822e08e0a675c1043d9446e8e0ab
Akhyar <ahyar12324@gmail.com>
2026-06-21 00:08:42 +0700

    Force single Apache MPM at startup

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,8 @@ set -e
 
 sed -i "s/Listen 80/Listen ${PORT:-8080}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:10000>/<VirtualHost *:${PORT:-8080}>/" /etc/apache2/sites-available/000-default.conf
+rm -f /etc/apache2/mods-enabled/mpm_event.* /etc/apache2/mods-enabled/mpm_worker.*
+a2enmod mpm_prefork rewrite headers >/dev/null
 
 php artisan config:clear
 php artisan route:clear
