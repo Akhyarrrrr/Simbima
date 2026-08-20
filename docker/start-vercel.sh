aa2a60e8e70773e4ca0ac49b596dff8de1a749ed
akhyarrrrr <ahyar12324@gmail.com>
2026-08-20 13:49:07 +0700

    Fix Vercel container startup

--- docker/start-vercel.sh
+++ docker/start-vercel.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
+APP_ROOT=/tmp/simbima/app
+rm -rf "$APP_ROOT"
+mkdir -p "$APP_ROOT"
+tar --exclude='./vendor' -C /var/www/html -cf - . | tar -C "$APP_ROOT" -xf -
+mkdir -p "$APP_ROOT/vendor"
+tar -C "$APP_ROOT/vendor" -xzf /opt/vendor.tar.gz
+cd "$APP_ROOT"
+
 mkdir -p "${VIEW_COMPILED_PATH:-/tmp/simbima/views}"
 export APP_CONFIG_CACHE=/tmp/simbima/config.php
 rm -f "$APP_CONFIG_CACHE" bootstrap/cache/config.php
