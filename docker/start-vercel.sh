1a4cc2690c16f3dca3ccf21d0e97e87ee4fff838
akhyarrrrr <ahyar12324@gmail.com>
2026-08-20 14:01:39 +0700

    Package Vercel app runtime

--- docker/start-vercel.sh
+++ docker/start-vercel.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-APP_ROOT=/tmp/simbima/app
-rm -rf "$APP_ROOT"
-mkdir -p "$APP_ROOT/vendor"
-tar -C "$APP_ROOT/vendor" -xzf /opt/vendor.tar.gz
-ln -s /var/www/html/app "$APP_ROOT/app"
-sed \
-    -e "s#__DIR__.'/vendor/autoload.php'#'$APP_ROOT/vendor/autoload.php'#" \
-    -e "s#__DIR__.'/bootstrap/app.php'#'/var/www/html/bootstrap/app.php'#" \
-    /var/www/html/artisan > "$APP_ROOT/artisan"
 cd /var/www/html
 
 mkdir -p "${VIEW_COMPILED_PATH:-/tmp/simbima/views}"
@@ -27,4 +18,4 @@ if [ -n "${MYSQL_CA_CERT_BASE64:-}" ]; then
     export MYSQL_ATTR_SSL_CA=/tmp/aiven-ca.pem
 fi
 
-exec php "$APP_ROOT/artisan" serve --host=0.0.0.0 --port="${PORT:-3000}"
+exec php -S 0.0.0.0:"${PORT:-3000}" -t /var/www/html/public /opt/simbima.phar
