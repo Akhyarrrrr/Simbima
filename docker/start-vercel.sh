f65abc5f838c3aaf47656f13d7b90e8fb7769d24
akhyarrrrr <ahyar12324@gmail.com>
2026-08-20 13:54:48 +0700

    Optimize Vercel vendor startup

--- docker/start-vercel.sh
+++ docker/start-vercel.sh
@@ -3,11 +3,14 @@ set -eu
 
 APP_ROOT=/tmp/simbima/app
 rm -rf "$APP_ROOT"
-mkdir -p "$APP_ROOT"
-tar --exclude='./vendor' -C /var/www/html -cf - . | tar -C "$APP_ROOT" -xf -
 mkdir -p "$APP_ROOT/vendor"
 tar -C "$APP_ROOT/vendor" -xzf /opt/vendor.tar.gz
-cd "$APP_ROOT"
+ln -s /var/www/html/app "$APP_ROOT/app"
+sed \
+    -e "s#__DIR__.'/vendor/autoload.php'#'$APP_ROOT/vendor/autoload.php'#" \
+    -e "s#__DIR__.'/bootstrap/app.php'#'/var/www/html/bootstrap/app.php'#" \
+    /var/www/html/artisan > "$APP_ROOT/artisan"
+cd /var/www/html
 
 mkdir -p "${VIEW_COMPILED_PATH:-/tmp/simbima/views}"
 export APP_CONFIG_CACHE=/tmp/simbima/config.php
@@ -24,4 +27,4 @@ if [ -n "${MYSQL_CA_CERT_BASE64:-}" ]; then
     export MYSQL_ATTR_SSL_CA=/tmp/aiven-ca.pem
 fi
 
-exec php artisan serve --host=0.0.0.0 --port="${PORT:-3000}"
+exec php "$APP_ROOT/artisan" serve --host=0.0.0.0 --port="${PORT:-3000}"
